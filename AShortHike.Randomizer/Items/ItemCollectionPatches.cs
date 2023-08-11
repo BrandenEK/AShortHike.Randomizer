53e17c7f25b430d44fb4f25185c9ef60268ddf1c
Branden Wood <brandenek03912@hotmail.com>
2023-08-11 13:33:11 -0400

    Add more dialog locations

--- AShortHike.Randomizer/Items/ItemCollectionPatches.cs
+++ AShortHike.Randomizer/Items/ItemCollectionPatches.cs
@@ -50,14 +50,90 @@ namespace AShortHike.Randomizer.Items
             // Only give random item if the amount is positive
             if (args.Length < 2 || int.TryParse(args[1], out int amount) && amount > 0)
             {
-                string locationId = context.originalSpeaker.position.ToString();
+                Main.LogWarning("Receiving item from conversation: " + context.originalSpeaker.name + ", " + context.originalSpeaker.position.ToString() + ", " + args[0]);
+                string locationId = CalculateNewLocationId(context.originalSpeaker.name, args[0]);
                 if (!Main.Randomizer.Items.IsLocationRandomized(locationId))
                     return;
 
-                Main.LogWarning("Giving item from conversation: " + locationId);
-                Main.Randomizer.Connection.SendLocation(locationId);
-
                 args = new string[] { "Stick", "0", "false" };
+                Main.Randomizer.Connection.SendLocation(locationId);
+            }
+
+            string CalculateNewLocationId(string locationId, string itemId)
+            {
+                float currentLevel;
+                switch (locationId)
+                {
+                    case "CampRangerNPC": // Visitor's center shop salesman
+                        {
+                            currentLevel = Singleton<GlobalData>.instance.gameData.tags.GetFloat("$FeathersSold");
+                            if (itemId == "GoldenFeather")
+                                return locationId + $"[{currentLevel}]";
+                            else if (itemId == "ParkHat")
+                                return locationId + "[9]";
+                            break;
+                        }
+                    case "ToughBirdNPC (1)": // Tough bird salesman
+                        {
+                            currentLevel = Singleton<GlobalData>.instance.gameData.tags.GetFloat("$ToughBirdSales");
+                            if (itemId == "GoldenFeather")
+                                return locationId + $"[{currentLevel}]";
+                            else if (itemId == "Watch")
+                                return locationId + "[9]";
+                            break;
+                        }
+                    case "VolleyballOpponent": // Beachstickball
+                        {
+                            if (itemId == "GoldenFeather")
+                                return locationId + "[0]";
+                            else if (itemId == "Coin")
+                                return locationId + "[1]";
+                            else if (itemId == "KidHat")
+                                return locationId + "[2]";
+                            break;
+                            //int current = 0;
+                            //if (Singleton<GlobalData>.instance.gameData.tags.GetBool("$GotVolleyPrize2"))
+                            //    current = 2;
+                            //else if (Singleton<GlobalData>.instance.gameData.tags.GetBool("$GotVolleyPrize"))
+                            //    current = 1;
+                            //return locationId + $"[{current}]";
+                        }
+                    case "FishBuyer": // Fisherman
+                        {
+                            if (itemId == "FishEncyclopedia")
+                                return locationId + "[0]";
+                            else if (itemId == "GoldenFishingRod")
+                                return locationId + "[1]";
+                            break;
+                        }
+                    case "CamperNPC": // Camper bribe
+                        {
+                            // Need to do something with (missingpermit)
+                            return locationId + "[5]";
+                        }
+                    case "Bunny_WalkingNPC (1)": // Racing bunny
+                        {
+                            if (itemId == "RunningShoes")
+                                return locationId + "[0]";
+                            else if (itemId == "Headband")
+                                return locationId + "[1]";
+                            break;
+                        }
+                    case "RaceOpponent": // Parkour racer
+                        {
+                            string race = "???"; // Get current race
+                            currentLevel = race == "peak" ? 2 : (race == "old build" ? 1 : 0);
+                            if (itemId == "Medal")
+                                return locationId + $"[{currentLevel}]";
+                            else if (itemId == "WalkieTalkie")
+                                return locationId + "[9]";
+                            break;
+                        }
+                    default:
+                        return locationId + "[0]";
+                }
+
+                return locationId;
             }
         }
     }
@@ -86,6 +162,24 @@ namespace AShortHike.Randomizer.Items
         public static void Postfix(string tag, int num)
         {
             Main.Log($"Saving int: {tag} ({num})");
+        }
+    }
+
+    [HarmonyPatch(typeof(Tags), nameof(Tags.SetString))]
+    class Tags_SaveString_Patch
+    {
+        public static void Postfix(string tag, string value)
+        {
+            Main.Log($"Saving string: {tag} ({value})");
+        }
+    }
+
+    [HarmonyPatch(typeof(Tags), nameof(Tags.SetFloat))]
+    class Tags_SaveFloat_Patch
+    {
+        public static void Postfix(string tag, float number)
+        {
+            Main.Log($"Saving float: {tag} ({number})");
         }
     }
 }
