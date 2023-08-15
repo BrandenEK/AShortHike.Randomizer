6cde1f9976cf1bc7f1eac91dfb2fbb9e484b1c65
Branden Wood <brandenek03912@hotmail.com>
2023-08-15 14:29:11 -0400

    Try to change to yarnglobaladapter

--- AShortHike.Randomizer/Items/ItemPreventionPatches.cs
+++ AShortHike.Randomizer/Items/ItemPreventionPatches.cs
@@ -16,6 +16,18 @@ namespace AShortHike.Randomizer.Items
         }
     }
 
+
+    [HarmonyPatch(typeof(DialogueController), nameof(DialogueController.StartConversation))]
+    class Dialog_Start_Patch
+    {
+        public static IConversation Conversation { get; private set; }
+
+        public static void Postfix(IConversation __result)
+        {
+            Conversation = __result;
+        }
+    }
+
     /// <summary>
     /// To prevent npcs not giving you the location check if you already have the associated item,
     /// hide what items you have based on if you have the location yet
@@ -23,25 +35,49 @@ namespace AShortHike.Randomizer.Items
     [HarmonyPatch(typeof(YarnFunctions), nameof(YarnFunctions.HasItem))]
     class Dialog_CheckItem_Patch
     {
-        public static void Postfix(ref bool __result, Value itemName, Value amount)
+        public static void Postfix(ref bool __result, IConversation context, Value itemName, Value amount)
         {
-            Main.LogWarning($"Checking if {itemName.AsString} is greater than {amount.AsNumber}");
             Tags tags = Singleton<GlobalData>.instance.gameData.tags;
+            string person = Dialog_Start_Patch.Conversation.originalSpeaker.name;
+            string item = itemName.AsString;
+            Main.LogWarning($"{person} is checking for item: {item}");
 
-            if (itemName.AsString == "CampingPermit")
+            if (person == "CamperNPC" && item == "CampingPermit")
             {
                 // If you already have the permit before first talking to the camper guy
                 __result = __result && tags.GetBool("Opened_CamperNPC[0]");
             }
-            else if (itemName.AsString == "Compass")
+            else if (person == "Fox_WalkingNPC" && item == "Compass")
             {
                 // If you already have the compass before talking to the compass guy
                 __result = tags.GetBool("Opened_Fox_WalkingNPC[0]");
             }
-            else if (itemName.AsString == "WalkieTalkie")
+            else if (person == "RaceOpponent" && item == "WalkieTalkie")
             {
                 // If you already have the walkie talkie before the parkour races
                 __result = tags.GetBool("Opened_RaceOpponent[9]");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(YarnToGlobalDataAdapter), nameof(YarnToGlobalDataAdapter.GetValue))]
+    class Dialog_GetValue_Patch
+    {
+        public static void Postfix(ref Value __result, string variableName)
+        {
+            Tags tags = Singleton<GlobalData>.instance.gameData.tags;
+            string person = Dialog_Start_Patch.Conversation.originalSpeaker.name;
+            string flag = variableName;
+            Main.LogWarning($"{person} is checking for flag: {flag}");
+
+            if (flag == "MissingPermit")
+            {
+                // If you have already returned the permit before catching the lake fish
+                __result = new Value(tags.GetBool("Opened_CamperNPC[0]") && !tags.GetBool("Opened_Player[0]"));
+            }
+            else if (person == "Turtle_WalkingNPC" && flag == "$BunnyQuestDone")
+            {
+
             }
         }
     }
@@ -55,16 +91,22 @@ namespace AShortHike.Randomizer.Items
     {
         public static void Postfix(ref bool __result, string tag)
         {
-            Main.LogWarning("Checking for flag: " + tag);
             Tags tags = Singleton<GlobalData>.instance.gameData.tags;
+            Main.LogWarning("Checking for flag: " + tag);
 
             if (tag == "MissingPermit")
             {
                 // If you have already returned the permit before catching the lake fish
                 __result = tags.GetBool("Opened_CamperNPC[0]") && !tags.GetBool("Opened_Player[0]");
             }
+            else if (tag == "$BunnyQuestDone")
+            {
 
-            //$bunnyquestdone
+            }
+
+            //$BunnyGiftHeadband
+
+            //
             //$bunnygiftheadband
             //bunnygotheadband
         }
