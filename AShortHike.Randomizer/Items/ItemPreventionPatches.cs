fe2d99e9b826952a04dd1458c97f654ebdc6df25
Branden Wood <brandenek03912@hotmail.com>
2023-08-16 13:28:10 -0400

    Allow kid to ride the boat once you have the key

--- AShortHike.Randomizer/Items/ItemPreventionPatches.cs
+++ AShortHike.Randomizer/Items/ItemPreventionPatches.cs
@@ -17,6 +17,31 @@ namespace AShortHike.Randomizer.Items
     }
 
     /// <summary>
+    /// Whenever a conversation is started, it stores the speaker name so that item preventions can tell what flags to change
+    /// </summary>
+    [HarmonyPatch(typeof(DialogueController), nameof(DialogueController.StartConversation))]
+    class Dialog_Start_Patch
+    {
+        private static IConversation _lastConversation;
+
+        public static void Postfix(IConversation __result)
+        {
+            _lastConversation = __result;
+        }
+
+        public static string CurrentConversation
+        {
+            get
+            {
+                if (_lastConversation == null || !_lastConversation.isAlive)
+                    return string.Empty;
+
+                return _lastConversation.originalSpeaker.name;
+            }
+        }
+    }
+
+    /// <summary>
     /// To prevent npcs not giving you the location check if you already have the associated item,
     /// hide what items you have based on if you have the location yet
     /// </summary>
@@ -68,23 +93,39 @@ namespace AShortHike.Randomizer.Items
         public static void Postfix(ref bool __result, string tag)
         {
             Tags tags = Singleton<GlobalData>.instance.gameData.tags;
-            Main.LogWarning("Checking for flag: " + tag);
+            string person = Dialog_Start_Patch.CurrentConversation;
+            string flag = tag;
+            Main.LogWarning($"{person} is checking for flag: {flag}");
 
-            if (tag == "MissingPermit")
+            if (person == string.Empty && tag == "MissingPermit")
             {
                 // If you have already returned the permit before catching the lake fish
                 __result = tags.GetBool("Opened_CamperNPC[0]") && !tags.GetBool("Opened_Player[0]");
             }
-            else if (tag == "$BunnyQuestDone" || tag == "$BunnyGiftHeadband" || tag == "BunnyGotHeadband")
+            else if (person == "Turtle_WalkingNPC" && (tag == "$BunnyQuestDone" || tag == "$BunnyGiftHeadband" || tag == "BunnyGotHeadband"))
             {
                 // If you already returned the headband to the rabbit before talking to the turtle
                 __result = __result && tags.GetBool("Opened_Turtle_WalkingNPC[0]");
             }
-            else if (tag == "$RabbitQuest")
+            else if (person == "Turtle_WalkingNPC" && tag == "$RabbitQuest")
             {
                 // If you already returned the headband to the rabbit before talking to the turtle
                 __result = __result || tags.GetBool("Opened_Bunny_WalkingNPC (1)[0]");
             }
+            else if (person == "StandingNPC (1)" && flag == "$RentedBoat")
+            {
+                // Allow the kid to go on the boat once you have the key (Not working - checks something else first)
+                __result = Singleton<GlobalData>.instance.gameData.GetCollected(CollectableItem.Load("BoatKey")) > 0;
+            }
         }
+    }
+
+    /// <summary>
+    /// Always return true so that the GetBool patch can always be applied
+    /// </summary>
+    [HarmonyPatch(typeof(Tags), nameof(Tags.HasBool))]
+    class Tags_HasFlag_Patch
+    {
+        public static void Postfix(ref bool __result) => __result = true;
     }
 }
