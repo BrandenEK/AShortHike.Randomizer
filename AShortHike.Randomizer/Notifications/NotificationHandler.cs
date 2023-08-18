a7fe3cdb8dc9300121ddd955d794fafbf85bbaef
Branden Wood <brandenek03912@hotmail.com>
2023-08-17 20:07:53 -0400

    Acually add handler

---
+++ AShortHike.Randomizer/Notifications/NotificationHandler.cs
@@ -0,0 +1,29 @@
+﻿using AShortHike.Randomizer.Items;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AShortHike.Randomizer.Notifications
+{
+    public class NotificationHandler
+    {
+
+        public void test()
+        {
+            UI ui = Singleton<ServiceLocator>.instance.Locate<UI>(false);
+            ItemPrompt prompt = ui.CreateItemPrompt(ItemCreator.CreateReceivedItem("Golden Feather", "Other"));
+            GameObject obj = prompt.gameObject;
+            obj.GetComponentInChildren<Image>().enabled = false;
+            RectTransform rect = obj.GetComponent<RectTransform>();
+            rect.anchorMin = Vector2.one;
+            rect.anchorMax = Vector2.one;
+            rect.anchoredPosition = new Vector2(-100, -100);
+            Object.Destroy(prompt);
+        }
+
+        public void UpdateNotifications()
+        {
+
+        }
+    }
+}
